fb25d1a373abd4bbe393e386479cfa18f376e4f2
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2020-09-28 14:45:10 +0530

    Program to create a linked ist

---
+++ Circular_Linked_List.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int info;
+    struct node *link;
+};
+
+struct node *insertValue(struct node *, int);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    head->link = NULL;
+    struct node *temp;
+    for (int i = 0; i < n; i++)
+    {
+        int value;
+        scanf("%d", &value);
+        if (i == 0)
+        {
+            head->info = value;
+            temp = head;
+        }
+        else
+        {
+            struct node *new = (struct node *)malloc(sizeof(struct node));
+            new->info = value;
+            new->link = NULL;
+            temp->link = new;
+            temp = temp->link;
+        }
+    }
+    temp->link = head;
+    int k;
+    scanf("%d", &k);
+
+    return 0;
+}
+
+struct node *insertValue(struct node *head, int k)
+{
+    struct node *new = (struct node *)malloc(sizeof(struct node *));
+    new->info = k;
+    new->link = NULL;
+
+    if (head == NULL)
+    {
+        new->link = new;
+        return new;
+    }
+
+    struct node* last=head;
+    while(last->link!=head){
+        last=last->link;
+    }
+    //Insertion in beginning
+    
+}
---
+++ DS assignment 3/Program_to_Create_a_linked_list.c
@@ -0,0 +1,88 @@
+//Write a program to create a linkedList
+
+//Name-Sai Prashant Saxena
+//Roll no-02220902719
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int info;
+    struct node *link;
+};
+void printList(struct node *);
+
+int main()
+{
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    struct node *temp = head;
+    printf("Hello there!!");
+    printf("\nWant to create a linked list press Y.....\n\t\t");
+    char c;
+    scanf("%c", &c);
+    if (c == 'Y' || c == 'y')
+    {
+        printf("Enter the value of head\n  ");
+        int value;
+        scanf("%d", &value);
+        head->info = value;
+        head->link = NULL;
+        temp = head;
+    }
+    else
+    {
+        printf("Why did you ran this program if you did not wanted to make a linked list\n");
+        printf("Program exited");
+        goto end;
+    }
+
+    int ch;
+    printf("Want to add more values to linked list press 1...\n  ");
+    scanf("%d", &ch);
+    if (ch == 1)
+    {
+    addNode:
+        printf("Enter the next value\n  ");
+        int value;
+        scanf("%d", &value);
+        struct node *new = (struct node *)malloc(sizeof(struct node));
+        new->info = value;
+        new->link = NULL;
+        temp->link = new;
+        temp = temp->link;
+        printf("\nThe new List is:");
+        printList(head);
+    }
+    else
+    {
+        printf("At last the linked list is..");
+        printList(head);
+        goto end;
+    }
+    printf("Want to add more values to linked list press 1...\n  ");
+    scanf("%d", &ch);
+    if (ch == 1)
+    {
+        goto addNode;
+    }
+    else
+    {
+        printf("At last the linked list is..");
+        printList(head);
+    }
+
+end:
+    return 0;
+}
+void printList(struct node *head)
+{
+    struct node *temp = head;
+    printf("\n");
+    while (temp != NULL)
+    {
+        printf("%d ", temp->info);
+        temp = temp->link;
+    }
+    printf("\n");
+}
